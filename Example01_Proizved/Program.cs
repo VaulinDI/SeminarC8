f6bd7a0c6668605d55d7c28e0e1ec64195f3b005
VaulinDI <vdi85@mail.ru>
2022-09-04 16:21:57 +0300

    proizved task solved

---
+++ Example01_Proizved/Program.cs
@@ -0,0 +1,39 @@
+﻿// Найти произведение двух матриц
+int m = 2;
+int n = 2;
+int[,] array = new int[m, n];
+int[,] array2 = new int[m, n];
+int[,] multimatrix = new int[m, n];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(0, 10);
+        Console.Write("{0}\t", array[i, j]);
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int i = 0; i < array2.GetLength(0); i++)
+{
+    for (int j = 0; j < array2.GetLength(1); j++)
+    {
+        array2[i, j] = new Random().Next(0, 10);
+        Console.Write("{0}\t", array2[i, j]);
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array2.GetLength(1); j++)
+    {
+        for (int k = 0; k < array2.GetLength(0); k++)
+        {
+            multimatrix[i,j] += array[i,k] * array2[k,j];
+        }
+    Console.Write("{0}\t", multimatrix[i, j]);
+    }
+    Console.WriteLine();
+}
