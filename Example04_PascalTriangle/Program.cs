ab9c6fade6dfd2b095785643e56df5add54bdac6
VaulinDI <vdi85@mail.ru>
2022-09-04 17:49:13 +0300

    pascal task solved

---
+++ Example04_PascalTriangle/Program.cs
@@ -0,0 +1,28 @@
+﻿// Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника
+int n = 5;
+int[][] triangle = new int[n][];
+
+triangle[0] = new int[] { 1 };
+ 
+for (int i = 1; i < triangle.Length; i++)
+{
+    triangle[i] = new int[i + 1];  
+    for (int j = 0; j <= i; j++)
+    {
+        if (j == 0 || j == i)
+            triangle[i][j] = 1;
+        else
+        {
+            triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+        }
+    }
+}
+
+for (int i = 0; i < triangle.Length; i++)
+{
+    for (int j = 0; j < triangle[i].Length; j++)
+    {
+        Console.Write("{0,-3} ", triangle[i][j]);
+    }
+    Console.WriteLine();
+}
